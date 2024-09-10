79a3cd48a6974e33afcc000b5c4012cd83487f13
Michele Angeletti <m.angeletti99@gmail.com>
2024-09-10 12:51:52 +0200

    Esercizio

---
+++ src/main/java/com/example/esspringrepositories2/EsSpringRepositories2Application.java
@@ -0,0 +1,13 @@
+package com.example.esspringrepositories2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsSpringRepositories2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsSpringRepositories2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esspringrepositories2/EsSpringRepositories2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esspringrepositories2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsSpringRepositories2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
